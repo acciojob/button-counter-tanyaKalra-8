a1250ada20eb142c75ede393b961ab7a18104531
Taniya <tanya.kalra200@gmail.com>
2023-07-11 21:57:19 +0530

    created

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [val,setval] = useState(0);
+
+  function counter(){
+    return setval(val++);
+  }
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Button clicked <span>{val}</span> times</p>
+        <button onClick={counter}>Click me</button>
     </div>
   )
 }
